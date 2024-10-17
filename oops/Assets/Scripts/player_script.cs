8f1174afa8053dbdfde2f4923e5c7f24a9f92400
augustinjanuary <0701brian04@gmail.com>
2024-10-17 15:03:12 +0100

    Code is broken but had to push
    Trying to get window borders to Clamp. Will figure out later

--- oops/Assets/Scripts/player_script.cs
+++ oops/Assets/Scripts/player_script.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class player_script : MonoBehaviour
 { 
-    float speed = 5.0f;
+    public int jumpDistance = 50;
+    public int cooldown = 100;
+    public float speed = 5.0f;
+
     float angle;
+
+    Vector3 direction;
     Vector3 mousePos;
     Vector3 object_pos;
-    Vector3 direction;
-    int jumpDistance = 50;
-    int cooldown = 100;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,30 +24,46 @@ public class player_script : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(cooldown != 0){
-            cooldown--;
-        } 
-        else if (cooldown < 0){
+
+        float hori = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+        
+        
+        
+
+        if(cooldown < 0){
             cooldown = 0;
+        } 
+        else if (cooldown != 0){
+            cooldown--;
         } 
 
         mousePos = Input.mousePosition;
-        direction = new Vector3(speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime, speed * Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        direction = new Vector3(speed * hori * Time.deltaTime, speed * vert * Time.deltaTime);
         transform.position += direction;
+
+        float posX = Mathf.Clamp(transform.position.x, 0, Screen.width/2);
+        float posY = Mathf.Clamp(transform.position.y, 0, Screen.height/2);
+
+        transform.position = Camera.main.WorldToScreenPoint((new Vector3(posX, posY, 1f));
 
         mousePos.z = 5.23f; //The distance between the camera and object
 	    object_pos = Camera.main.WorldToScreenPoint(transform.position);
         mousePos.x = mousePos.x - object_pos.x;
 	    mousePos.y = mousePos.y - object_pos.y;
 
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+	    transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
         if((Input.GetAxisRaw("Fire3") != 0) && (direction != Vector3.zero) && (cooldown == 0)){
             transform.position = transform.position + direction * jumpDistance;
             cooldown = 100;
         }
 
-        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-	    transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
+        if(Input.GetAxisRaw("Jump") != 0){
+            Debug.Log(Camera.main.WorldToScreenPoint(transform.position));
+        }
     }
 }
+
 

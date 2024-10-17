b171b0f594e6ba50ce07168a703ea1a1795d7803
augustinjanuary <0701brian04@gmail.com>
2024-10-17 22:29:09 +0100

    FINALLY got screen bounds
    Took some fiddling. Also allows me to zoom out and keep the same border. pog

--- oops/Assets/Scripts/player_script.cs
+++ oops/Assets/Scripts/player_script.cs
@@ -13,23 +13,20 @@ public class player_script : MonoBehaviour
     Vector3 direction;
     Vector3 mousePos;
     Vector3 object_pos;
+    public Vector3 screenBounds;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         cooldown = 0;
+        CalculateScreenBoundaries();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        float hori = Input.GetAxisRaw("Horizontal");
-        float vert = Input.GetAxisRaw("Vertical");
-        
-        
-        
 
         if(cooldown < 0){
             cooldown = 0;
@@ -39,15 +36,18 @@ public class player_script : MonoBehaviour
         } 
 
         mousePos = Input.mousePosition;
-        direction = new Vector3(speed * hori * Time.deltaTime, speed * vert * Time.deltaTime);
+        direction = new Vector3(speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime, speed * Input.GetAxisRaw("Vertical") * Time.deltaTime);
         transform.position += direction;
 
-        float posX = Mathf.Clamp(transform.position.x, 0, Screen.width/2);
-        float posY = Mathf.Clamp(transform.position.y, 0, Screen.height/2);
+        Vector3 transPos = transform.position;
 
-        transform.position = Camera.main.WorldToScreenPoint((new Vector3(posX, posY, 1f));
+        transPos.x = Mathf.Clamp(transPos.x, screenBounds.x*-1, screenBounds.x);
+        transPos.y = Mathf.Clamp(transPos.y, screenBounds.y*-1, screenBounds.y);
+
+        transform.position = transPos;
 
         mousePos.z = 5.23f; //The distance between the camera and object
+
 	    object_pos = Camera.main.WorldToScreenPoint(transform.position);
         mousePos.x = mousePos.x - object_pos.x;
 	    mousePos.y = mousePos.y - object_pos.y;
@@ -55,14 +55,39 @@ public class player_script : MonoBehaviour
         angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 	    transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-        if((Input.GetAxisRaw("Fire3") != 0) && (direction != Vector3.zero) && (cooldown == 0)){
+
+
+        if ((Input.GetAxisRaw("Fire3") != 0) && (direction != Vector3.zero) && (cooldown == 0))
+        {
+            Debug.Log(direction);
+            Debug.Log(direction * jumpDistance);
             transform.position = transform.position + direction * jumpDistance;
             cooldown = 100;
         }
 
-        if(Input.GetAxisRaw("Jump") != 0){
-            Debug.Log(Camera.main.WorldToScreenPoint(transform.position));
+        else if ((Input.GetAxisRaw("Fire1") != 0) && (cooldown == 0))
+        {
+            Debug.Log(Vector3.ClampMagnitude(new Vector3(mousePos.x, mousePos.y, 0f).normalized * jumpDistance, 3.5f));
+            transform.position = transform.position + Vector3.ClampMagnitude(new Vector3(mousePos.x, mousePos.y, 0f).normalized * jumpDistance, 5.0f);
+            cooldown = 100;
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Camera.main.orthographicSize += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Camera.main.orthographicSize += 1;
+            CalculateScreenBoundaries();
+        }
+
+    }
+
+    void CalculateScreenBoundaries()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 1f));
     }
 }
 
